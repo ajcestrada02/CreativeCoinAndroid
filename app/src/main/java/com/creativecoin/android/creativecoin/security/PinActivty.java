9bcc5bba5e960c54111fdc3c0e7991bb8667ccad
Amelito Estrada <x209486@corp.ads>
2019-12-15 00:30:56 +0800

    rating progress bar action Listener

--- app/src/main/java/com/creativecoin/android/creativecoin/security/PinActivty.java
+++ app/src/main/java/com/creativecoin/android/creativecoin/security/PinActivty.java
@@ -1,9 +1,14 @@
 package com.creativecoin.android.creativecoin.security;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 import com.creativecoin.android.creativecoin.MainActivity;
@@ -13,12 +18,10 @@ public class PinActivty extends AppCompatActivity {
 
     private static final String tempPinCode = "1214";
     private static String enteredPinCode = "";
-
+    private int rate = 0;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pin_main);
-
-
     }
 
     protected boolean isPinCorrect(String entPincode){
@@ -29,33 +32,53 @@ public class PinActivty extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.pinNumber0:
                 this.enteredPinCode += "0";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinNumber1:
                 this.enteredPinCode += "1";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinNumber2:
                 this.enteredPinCode += "2";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinNumber3:
                 this.enteredPinCode += "3";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinNumber4:
                 this.enteredPinCode += "4";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinNumber5:
                 this.enteredPinCode += "5";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinNumber6:
                 this.enteredPinCode += "6";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinNumber7:
                 this.enteredPinCode += "7";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinNumber8:
                 this.enteredPinCode += "8";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinNumber9:
                 this.enteredPinCode += "9";
+                rate++;
+                addRatingListener(rate);
                 break;
             case R.id.pinClear:
                 pinClear();
@@ -72,15 +95,40 @@ public class PinActivty extends AppCompatActivity {
                 pinClear();
             }else{
                 Toast.makeText(this, "Incorrect pin code", Toast.LENGTH_SHORT).show();
-                pinClear();
+
+                // Adding delay to clear the pin rating bar icon
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        pinClear();
+                    }
+                },500);
+
             }
         }
 
     }
 
     private void pinClear(){
+        rate=0;
+        addRatingListener(rate);
         this.enteredPinCode = "";
     }
 
+    private void addRatingListener(int rate){
+        RatingBar ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        ratingBar.setRating(rate);
+        /*ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Toast.makeText(PinActivty.this,
+                        "Rating changed, current rating "+ ratingBar.getRating(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+
+    }
 }
 
